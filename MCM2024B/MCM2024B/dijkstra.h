1eb0184a4b4cf721ec622148488f372282c88fc5
5trawberryf <138018380+5trawberryf@users.noreply.github.com>
2024-01-19 21:13:55 +0800

    21:13

--- MCM2024B/MCM2024B/dijkstra.h
+++ MCM2024B/MCM2024B/dijkstra.h
@@ -8,32 +8,34 @@ inline void add_edge(int from, int to, double length, vector<Edge>* adj)
 inline int dijkstra(int s, int t, int numv, vector<Edge>* adj)//D(s,t)
 {
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-	bool visited[maxV];
 	int d[maxV];
+	bool visited[maxV];
 	for (int i = 1; i <= numv; i++)
 		d[i] = maxD;
 	memset(visited, 0, sizeof(visited));
 	d[s] = 0;
 	int mind = maxD;
 	int now = 0;
+	q.push({ 0, s });
 	while (1) {
 		mind = maxD;
-		for (int i = 1; i <= numv; i++) {
-			if (mind > d[i] && !visited[i])
-			{
-				mind = d[i];
-				now = i;
-			}
-		}
+		auto x = q.top();
+		q.pop();
+		int now = x.second, mind = x.first;
+		if (visited[now])
+			continue;
 		if (mind == maxD)
 			return d[t];
 		if (now == t)
 			return d[t];
 		visited[now] = 1;
 		for (int i = 0; i < adj[now].size(); i++) {
-			int to = adj[now][i].end;
+			int to = adj[now][i].start;
 			if (d[now] + adj[now][i].weight < d[to])
+			{
 				d[to] = d[now] + adj[now][i].weight;
+				q.push({ d[to], to });
+			}
 		}
 	}
 
