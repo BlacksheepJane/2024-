aa9565b0e02e1a7d05d34ec714399ef970111cc1
5trawberryf <138018380+5trawberryf@users.noreply.github.com>
2024-01-20 10:58:21 +0800

    pagerank

--- MCM2024B/MCM2024B/dijkstra.h
+++ MCM2024B/MCM2024B/dijkstra.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "base.h"
 
-inline void add_edge(int from, int to, double length, vector<Edge>* adj)
+inline void add_Edge(int from, int to, double length, vector<Edge>* adj)
 {
 	adj[from].push_back(Edge{ from, to, length });
 }
@@ -30,7 +30,7 @@ inline int dijkstra(int s, int t, int numv, vector<Edge>* adj)//D(s,t)
 			return d[t];
 		visited[now] = 1;
 		for (int i = 0; i < adj[now].size(); i++) {
-			int to = adj[now][i].start;
+			int to = adj[now][i].end;
 			if (d[now] + adj[now][i].weight < d[to])
 			{
 				d[to] = d[now] + adj[now][i].weight;
@@ -39,4 +39,43 @@ inline int dijkstra(int s, int t, int numv, vector<Edge>* adj)//D(s,t)
 		}
 	}
 
+}
+void Page_Rank(vector<Edge>* adj, vector<Edge>* antiadj, int numv, double* p,double a) {
+	vector <Edge>new_Adj[maxV];
+	vector <Edge>new_Antiadj[maxV];
+	for (int i = 0; i < numv; i++) {
+		double pr = 0;
+		for (int k = 0; k < antiadj[i].size(); k++)
+			pr += antiadj[i][k].weight;
+		pr *= a;
+		pr += p[i];
+		pr /= adj[i].size();
+		for (int j = 0; j < adj[i].size(); j++)
+		{
+			add_Edge(i, adj[i][j].end, pr, new_Adj);
+			add_Edge(adj[i][j].end, i, pr, new_Antiadj);
+		}
+	}
+	for (int i = 0; i < numv; i++)
+	{
+		for (int j = 0; j < adj[i].size(); j++)
+			adj[i][j] = new_Adj[i][j];
+		for (int j = 0; j < antiadj[i].size(); j++)
+			antiadj[i][j] = new_Antiadj[i][j];
+	}
+}
+void output(vector<Edge>* adj,int numv) {
+	std::ofstream myfile;
+	myfile.open("example.csv");
+	for (int i = 0; i < numv; i++)
+		for (int j = 0; j < adj[i].size(); j++)
+		{
+			myfile << i;
+			myfile << ",";
+			myfile << adj[i][j].end;
+			myfile << ",";
+			myfile << adj[i][j].weight;
+			myfile << ",\n";
+		}
+	myfile.close();
 }
