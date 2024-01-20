780e06aa4e4d26f5cdea728b979230548951eba4
5trawberryf <138018380+5trawberryf@users.noreply.github.com>
2024-01-20 15:34:38 +0800

    SA

--- MCM2024B/MCM2024B/dijkstra.h
+++ MCM2024B/MCM2024B/dijkstra.h
@@ -5,7 +5,7 @@ inline void add_Edge(int from, int to, double length, vector<Edge>* adj)
 {
 	adj[from].push_back(Edge{ from, to, length });
 }
-inline int dijkstra(int s, int t, int numv, vector<Edge>* adj)//D(s,t)
+inline int dijkstra(int s, int t, int numv, vector<Edge>* adj, unordered_map<pair<int, int>, Edge, pair_hash>& map)//D(s,t)
 {
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
 	int d[maxV];
@@ -29,14 +29,45 @@ inline int dijkstra(int s, int t, int numv, vector<Edge>* adj)//D(s,t)
 		visited[now] = 1;
 		for (int i = 0; i < adj[now].size(); i++) {
 			int to = adj[now][i].end;
-			if (d[now] + adj[now][i].weight < d[to])
+			if (d[now] + map[{now, to}].weight < d[to])
 			{
-				d[to] = d[now] + adj[now][i].weight;
+				d[to] = d[now] + map[{now, to}].weight;
 				q.push({ d[to], to });
 			}
 		}
 	}
 
+}
+struct pa {
+	double a, b, c;
+};
+double calculate_weight(pa parameters,double pr,double length,double line) {
+	double ans = int(parameters.a * pr + parameters.b * length + parameters.c * line);
+}
+double line(pair<int,int> e, vector<Point> v) {
+	double x1 = v[e.first].x;
+	double y1 = v[e.first].y;
+	double x2 = v[e.second].x;
+	double y2 = v[e.second].y;
+	double dy = 78.125 * (y1 - y2);
+	double dx = 85.714 * (x1 - x2);
+	return sqrt(dy * dy + dx * dx);
+}
+void cal_weight(pa now, unordered_map<pair<int, int>, Edge, pair_hash>& weight,unordered_map<pair<int, int>, Edge, pair_hash>& pr, unordered_map<pair<int, int>, Edge, pair_hash>& length, vector<Point> v) {
+	for (auto it : pr) {
+		weight[it.first].weight = calculate_weight(now, pr[it.first].weight, length[it.first].weight, line(it.first,v));
+	}
+}
+double update_parameter(double now) {
+	double dnow = rand() % 10000 / 10000-0.5;
+	return now + dnow;
+}
+pa update(pa now) {
+	pa ans;
+	ans.a = update_parameter(now.a);
+	ans.b = update_parameter(now.b);
+	ans.c = update_parameter(now.c);
+	return ans;
 }
 void Page_Rank(vector<Edge>* adj, vector<Edge>* antiadj, int numv, double* p,double a) {
 	vector <Edge>new_Adj[maxV];
